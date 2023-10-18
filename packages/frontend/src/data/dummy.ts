54309b68aeadc84fb85838232a9af1f3afa26ebe
0tuedon <90271995+0tuedon@users.noreply.github.com>
2023-10-18 12:00:02 +0100

    chore: setup landing page

---
+++ packages/frontend/src/data/dummy.ts
@@ -0,0 +1,14 @@
+export const navBarLinks = [
+    {
+        text: "Home",
+        link: "/"
+    },
+    {
+        text: "About us",
+        link: "/about"
+    },
+    {
+        text: "Testimonials ",
+        link: "/testimonials"
+    }
+]
--- packages/frontend/tailwind.config.ts
+++ packages/frontend/tailwind.config.ts
@@ -13,6 +13,29 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        primary:{
+          dark_green:"#007D51",
+          green:"#4CAF50",
+          orange:"#FFA500",
+          coral:"#FF6347",
+          blue:"#3498DB",
+          yellow:"#FFD700"
+        },
+       light:{
+          white:"#FFFFFF",
+          neutral:{
+            1:"#F8F8F8",
+            2:"#F4F4F4",
+            3:"#EDEDED",
+            4:"#DEDEDE",
+            5:"#BBBBBB",
+            6:"#999999",
+            7:"#777777"
+          }
+
+        }
+      }
     },
   },
   plugins: [],
